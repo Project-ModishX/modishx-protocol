0a2e673357f749e7ce2127062c5cfb907b955e06
Developer Uche <nweleuchenna2004@gmail.com>
2023-01-03 00:22:47 +0100

    developeruche: implemented marketplace storage

---
+++ contracts/modules/marketplace/providers/Dto.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library Dto {
+    struct MarketplaceSchema {
+        uint256 listing_fee;
+    }
+}
--- contracts/modules/marketplace/providers/marketplace.provider.sol
+++ contracts/modules/marketplace/providers/marketplace.provider.sol
@@ -1,7 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { Dto } from "./dto.sol";
+import { Positions } from "./positions.sol";
+import {Errors} from "./errors.sol";
+
 
 library MarketplaceProvider {
-    
+    function marketplaceStorage() internal pure returns (Dto.MarketplaceSchema storage ms) {
+        bytes32 position = Positions.MODISHX_MARKETPLACE_POSITION;
+        assembly {
+        ms.slot := position
+        }
+    }
 }
