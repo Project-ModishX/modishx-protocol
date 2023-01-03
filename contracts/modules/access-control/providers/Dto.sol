8a882b54413002d2702df6690325e40950f68a81
Developer Uche <nweleuchenna2004@gmail.com>
2023-01-03 10:47:17 +0100

    developeruche: added access control schema

---
+++ contracts/modules/access-control/providers/Dto.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+library Dto {
+  enum Roles {
+    DEFAULT, // This is the role that all addresses has by default 
+    MARKETPLACE_MANAGER;
+    AUCTION_ADMIN;
+    FUNDS_MANAGER;
+  }
+
+  struct AccessControlSchema {
+    mapping(address => Roles) role; // address => role
+    address superuser; // The superuser can preform all the role and assign role to addresses 
+    bool is_initialized;
+  }
+}
