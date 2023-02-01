c7d904b22c319a1b885364a46f630f0be989301e
Developer Uche <nweleuchenna2004@gmail.com>
2023-02-01 12:36:55 +0100

    developeruche: slot outlined

--- contracts/modules/marketplace/providers/Dto.sol
+++ contracts/modules/marketplace/providers/Dto.sol
@@ -6,14 +6,14 @@ import {PriceSourceController} from "../controllers/price-source.controller.sol"
 library Dto {
     struct MartketplaceItem {
         uint256 marketplace_item_id; // this is the id the marketplace uses to track it items 
+        uint256[] wearable_ids; // is is a list of token id of the nft to be listed 
+        uint256 price; // this is the price the nft would be sold for
+        uint256[] quantities; // this is the amount of the indivual items 
         address seller;
         address token_address; // this is the address of the nft contract 
-        uint256[] wearable_ids; // is is a list of token id of the nft to be listed 
-        uint256[] quantities; // this is the amount of the indivual items 
-        uint256 price; // this is the price the nft would be sold for
-        uint256 created_at; // this is the block.timestamp this token was placed for sale 
-        uint256 sold_at; // this is the block.timestamp this token wa sold 
-        uint256 reffer_percentage; // in 1 = 1%
+        uint40 created_at; // this is the block.timestamp this token was placed for sale 
+        uint40 sold_at; // this is the block.timestamp this token wa sold 
+        uint8 reffer_percentage; // in 1 = 1%
         bool is_sold; //this is a market to sold when a token is sold or not 
         bool is_cancelled; //  this is a pointer to show if a wearable listed for sale has been cancalled 
     }
