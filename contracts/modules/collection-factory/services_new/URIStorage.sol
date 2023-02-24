3458c6259a0075e6392c6140c1c69761bf40540d
Nwele Uchenna <nweleuchenna2004@gmail.com>
2023-02-24 17:16:51 +0100

    developeruche: installed the ERC721 service

---
+++ contracts/modules/collection-factory/services_new/URIStorage.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.8.0) (token/ERC1155/ERC1155.sol)
+
+pragma solidity ^0.8.0;
+
+
+
+// TODO: We would be making use of url-storage 
+
+
+contract URIStorage {
+
+}
